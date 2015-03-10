970ee165c9a0945485dabc3b0742798f64a345f1
Cristian Saita <acatias@gmail.com>
2015-03-10 21:21:06 +0100

    Test Eclipse commit!

---
+++ src/ghc/GoogleHashCode.java
@@ -0,0 +1,17 @@
+package ghc;
+
+public class GoogleHashCode {
+
+	public static void main(String[] args) {
+		
+		long startTime = System.currentTimeMillis();		
+		
+		System.out.println("Hello World!");
+		
+		long endTime = System.currentTimeMillis();
+		
+		System.out.println("Time: " + ((endTime - startTime) / 1000) + " seconds");
+
+	}
+
+}
