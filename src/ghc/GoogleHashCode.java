12fda790fd37d4dfadb971caef4d0feef6cbf829
Cristian Saita <acatias@gmail.com>
2015-03-10 21:47:54 +0100

    Commit

--- src/ghc/GoogleHashCode.java
+++ src/ghc/GoogleHashCode.java
@@ -1,16 +1,84 @@
 package ghc;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class GoogleHashCode {
 
+	public static void readFromFile(String fileName) {
+
+		try {
+			
+			BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
+			
+			String line = br.readLine();
+			
+			while (line != null) {
+				
+				System.out.println("String: " + line);
+				
+				if (line.charAt(0) == '#') {
+					
+					int i = Integer.parseInt(line.substring(16));
+					
+					System.out.println("Integer: " + i);
+				}
+				
+				line = br.readLine();
+			}
+
+			br.close();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void writeToFile(String fileName) {
+		
+		try {
+			
+			BufferedWriter bw = new BufferedWriter(new FileWriter(new File(fileName)));
+			
+			bw.write("# GoogleHashCode");
+			
+			bw.write("" + 2015);
+
+			bw.newLine();
+			
+			bw.write("Google Hash Code ");
+			
+			bw.write("" + 2015);
+
+			bw.newLine();			
+			
+			bw.close();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		
-		long startTime = System.currentTimeMillis();		
+		long startTime = System.nanoTime();		
 		
 		System.out.println("Hello World!");
 		
-		long endTime = System.currentTimeMillis();
+		readFromFile("README.txt");
 		
-		System.out.println("Time: " + ((endTime - startTime) / 1000) + " seconds");
+		writeToFile("WRITEME.txt");
+		
+		long endTime = System.nanoTime();
+		
+		System.out.println("Time: " + ((endTime - startTime) / 1000000.0) + " seconds");
 
 	}
 
